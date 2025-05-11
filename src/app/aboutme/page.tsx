034c3f691161fcc457eebdbb6dc4d55752f938a4
bloopotato <neoyuhui@gmail.com>
2025-05-11 14:19:32 +0800

    new

---
+++ src/app/aboutme/page.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import About from './About'
+import Projects from './Projects'
+import Contact from './Contact'
+import Links from './Links'
+
+const page = () => {
+  return (
+    <div>
+      hello
+      <About/>
+      <Projects/>
+      <Contact/>
+      <Links/>
+    </div>
+  )
+}
+
+export default page
